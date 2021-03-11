1eb52d774c4f5776f66bb83c12fc3e879769041a
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-03-11 19:32:22 +0100

    Add new listitem om button click

--- app.js
+++ app.js
@@ -1,42 +1,10 @@
-//Functions and parameters
-const anotherName = "Dev Ed";
-const youtuber = 'Traversy Media';
+const userList = document.querySelector(".name-list li");
+const listInput = document.querySelector(".list-input");
+const addListBtn = document.querySelector(".addListBtn");
 
-//Function 1
-function toUpper(text) {
-  const upperCased = text.toUpperCase();
-  console.log(upperCased);
-};
-
-toUpper(anotherName);
-toUpper(youtuber);
-
-//Function 2
-const toUpperCase = function() {
-  
-};
-
-//Function 3
-const toLowerCase = () => {
-  
-};
-
-// Variables
-var life = 100; // This is my life bar NUMBER
-life = life - 10;
-// 90
-life = life / 10;
-// 9
-
-const sum = 100; // This is my const variable. You can not set a new value to this variable like we did for life variable
-
-let anotherSum = 1000; // This is my let variable
-anotherSum = anotherSum - 500;
-
-var name = "Dev Ed"; // This is the name displayed in the game STRING
-
-var checkout = false; //BOOLEAN
-
-var box = null;
-
-console.log(anotherSum);
+addListBtn.addEventListener('click', function() {
+  const newLi = document.createElement("LI");
+  const liContent = document.createTextNode(listInput.value);
+  newLi.appendChild(liContent);
+  userList.appendChild(newLi);
+});
